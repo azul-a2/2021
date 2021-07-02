12d87444f116dff78780f8a6105292a6ec18a796
Gede Kitchen Batara <gkitbat12@gmail.com>
2021-07-02 10:21:57 +0700

    delete some
    ok

--- application/views/templates/testing_footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-	</main>
-	</body>
-	<script src="<?php echo base_url('assets/framework-css/bootstrap/dist/js/bootstrap.js') ?>"></script>
-	<script src="<?php echo base_url('assets/js/jquery.js') ?>"></script>
-    <script src="<?php echo base_url('assets/js/horizontal-scroll.js')?>"></script>
-	<script src="<?php echo base_url('assets/js/fixed-top.js') ?>"></script>
-</html>
